2e214d233e819c0ff64ff78d4a3f2510f07bf8c2
aaustin07 <aaustin07@outlook.com>
2022-08-23 21:28:00 -0500

    still working on 3

--- scripts_A.sql
+++ scripts_A.sql
@@ -33,4 +33,30 @@
 -- Order by p.height ASC
 --edward carl - st louis browns -- 1
 
+-- 3.Find all players in the database who played at Vanderbilt University. 
+-- Create a list showing each player’s first and last names as well as the total salary they earned in the major leagues. 
+-- Sort this list in descending order by the total salary earned. 
+-- Which Vanderbilt player earned the most money in the majors?
+
+--Tbls to pull from 
+-- A. collegeplaying --filter to Vandy in WHERE clause; column schoolid=vandy
+-- B. people --first & last name, 
+-- C. salaries --DECS in WHERE clause
+
+Select 
+p.namefirst,
+p.namelast,
+cp.schoolid,
+SUM(s.salary) AS total_sal
+
+From people p
+Inner Join collegeplaying cp 
+on p.playerid = cp.playerid
+Inner Join  salaries s
+on p.playerid = s.playerid
+
+WHERE 
+cp.schoolid = 'Vandy'
+Group By 1,2,3
+--Order By total_sal DESC
 
