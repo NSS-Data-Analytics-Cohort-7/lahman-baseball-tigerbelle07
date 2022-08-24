d4e84634b7d7f01dbc07ab79b7dbdd9fac405690
aaustin07 <aaustin07@outlook.com>
2022-08-23 20:07:26 -0500

    working on problem 2

---
+++ scripts_A.sql
@@ -0,0 +1,17 @@
+-- 1. What range of years for baseball games played does the provided database cover?
+-- SELECT 
+-- MIN(span_first) as first_game,
+-- MAX(span_last) as lastest_game
+-- FROM homegames
+-- first_game = 1871-05-04; latest_game = 2016-10-02
+
+-- 2.Find the name and height of the shortest player in the database. 
+-- How many games did he play in? What is the name of the team for which he played?
+-- A.Select * 
+-- From people--start here for name, ht and weight,
+--B.
+-- Select *
+-- From appearances--distinct count on playerid
+--C.
+-- Select *
+-- From teams--get team info..join on teamid and maybe yearid...pull in name column to get name of team. 
