17e3730c816dfe37f49439800a0dca49b9e9dbd9
aaustin07 <aaustin07@outlook.com>
2022-08-23 21:00:48 -0500

    finished 2...working on 3

--- scripts_A.sql
+++ scripts_A.sql
@@ -8,10 +8,29 @@
 -- 2.Find the name and height of the shortest player in the database. 
 -- How many games did he play in? What is the name of the team for which he played?
 -- A.Select * 
--- From people--start here for name, ht and weight,
+-- From people--start here for name, ht and weight,--join on playerid, 
 --B.
 -- Select *
 -- From appearances--distinct count on playerid
 --C.
 -- Select *
 -- From teams--get team info..join on teamid and maybe yearid...pull in name column to get name of team. 
+-- SELECT 
+-- p.namegiven,
+-- p.weight,
+-- p.height,
+-- t.name AS team_name,
+-- SUM(a.g_all) AS total_games
+
+-- From appearances a
+-- Inner Join people p
+-- on a.playerid = p.playerid
+-- Inner Join teams t
+-- on a.teamid = t.teamid
+-- and a.yearid = t.yearid
+
+-- Group By 1,2,3,4
+-- Order by p.height ASC
+--edward carl - st louis browns -- 1
+
+
