61255d74827c3e1b70b5178e6f596954407186fd
aaustin07 <aaustin07@outlook.com>
2022-08-27 13:52:39 -0500

    last push...added to 7...just not the percentage. Still working on 8

--- scripts_A.sql
+++ scripts_A.sql
@@ -167,6 +167,28 @@
 -- GROUP BY 1
 -- ORDER BY total_wins ASC
 
+-- SELECT 
+-- name,
+-- MIN(w) as total_wins --help from tyler
+-- FROM teams
+-- WHERE 
+-- WSWin = 'Y'
+-- and Yearid BETWEEN '1970' AND '2016'
+-- GROUP BY 1
+-- ORDER BY total_wins ASC
+
+-- SELECT 
+-- name,
+-- MIN(w) as total_wins --help from tyler
+-- FROM teams
+-- WHERE 
+-- WSWin = 'N'
+-- and Yearid BETWEEN '1970' AND '2016'
+-- GROUP BY 1
+-- ORDER BY total_wins ASC
+
+
+
 
 -- 8.Using the attendance figures from the homegames table, find the teams and parks which had the top 5 average attendance per game in 2016 (where average attendance is defined as total attendance divided by number of games). 
 -- Only consider parks where there were at least 10 games played. Report the park name, team name, and average attendance. 
