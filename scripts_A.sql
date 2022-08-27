e08d5fa34c6c8bf06708d71798852bfa1cb66821
aaustin07 <aaustin07@outlook.com>
2022-08-27 12:28:00 -0500

    still working on 8

--- scripts_A.sql
+++ scripts_A.sql
@@ -155,3 +155,14 @@
 -- and Yearid BETWEEN '1970' AND '2016'
 -- GROUP BY 1
 -- ORDER BY total_wins ASC
+
+-- 8.Using the attendance figures from the homegames table, find the teams and parks which had the top 5 average attendance per game in 2016 (where average attendance is defined as total attendance divided by number of games). 
+-- Only consider parks where there were at least 10 games played. Report the park name, team name, and average attendance. 
+-- Repeat for the lowest 5 average attendance.
+--possible tbs -homegames - attendance; games played. I need park info--parks also use homegames tbl to connect parks tbl. Don't need anything from homegames tbl. tbl --teams tbl for team info.
+
+SELECT 
+
+
+
+
