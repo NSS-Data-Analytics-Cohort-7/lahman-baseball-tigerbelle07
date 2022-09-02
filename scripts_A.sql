c781f60e6410dc16a7e23d55aaa944e7f10ad273
aaustin07 <aaustin07@outlook.com>
2022-09-01 20:53:02 -0500

    attempting 10...but out of steam

--- scripts_A.sql
+++ scripts_A.sql
@@ -330,28 +330,23 @@
 
 
 
-
-
-
-
 -- 10.Find all players who hit their career highest number of home runs in 2016. 
 -- Consider only players who have played in the league for at least 10 years, and who hit at least one home run in 2016. 
 -- Report the players' first and last names and the number of home runs they hit in 2016.
 --possible tbls - teams, & people..maybe appearances to join to people tbl.
 
--- SELECT 
--- p.namegiven,
--- EXTRACT(year from year) as Year,
--- SUM(t.g)
--- --Max(t.hr)
+SELECT 
+p.namegiven,
+SUM(t.g)
+--Max(t.hr)
 
--- FROM teams t
--- LEFT JOIN appearances a
--- ON t.yearid = a.yearid
--- LEFT JOIN people p
--- ON a.playerid = p.playerid
+FROM teams t
+LEFT JOIN appearances a
+ON t.yearid = a.yearid
+LEFT JOIN people p
+ON a.playerid = p.playerid
 
--- --WHERE
--- GROUP BY 1,2
+--WHERE
+GROUP BY 1
 
 
